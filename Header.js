517e4a2d9c897b4d05fbe67563e4482ba6aa2788
Arun.R <127499027+Arun-SoftwareDeveloper@users.noreply.github.com>
2023-05-10 20:26:09 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Header.css"
+
+const Header=function(){
+    return <div className="header">AWE ,Grab your Offer Now!!!</div>
+    
+}
+export default Header;
